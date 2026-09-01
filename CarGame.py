0e4f4b9f5ef787685fdab38333a5f3c06213fd28
Qazzal-Bisdt <600757@STudent.belgiumcampus.ac.za>
2026-09-01 12:30:09 +0200

    Update CarGame.py

--- CarGame.py
+++ CarGame.py
@@ -1,1 +1,22 @@
-
+print("Start , Stop, Help")
+started = false
+command = ""
+while True:
+  command = input(">> ").lower()
+  if command == "start":
+    if strated:
+      print("Car already started")
+    else: print("Car started...")
+  elif command == "stop":
+     if not started:
+       print("Car already stopped")
+     else: print("Car stopped...")
+  elif command == "help":
+    print("""
+  start - to start the car
+  stop - to stop the car
+  quit - to exit the program
+    """)
+  elif command == "quit":
+    break
+  else: print("Sorry, I dont understand that")
