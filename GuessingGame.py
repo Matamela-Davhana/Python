692d90a4396fe589b2343b00c1ba40f49ba3a1a1
Qazzal-Bisdt <600757@STudent.belgiumcampus.ac.za>
2026-09-01 12:16:36 +0200

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,1 +1,10 @@
-secret_number = 
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+  guess = int(input("Guess number: "))
+  guess_count += 1
+  if guess == secret_number:
+    print('You guessed right')
+    break
+else: print('Sorry you failed')
